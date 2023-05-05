0e08d8cd4b95fde20c9b37f85290904a1a269159
lucweytingh <lucweytingh321@gmail.com>
2023-05-05 13:39:36 +0200

    Updated readme

--- scripts/raspberry_pi_on_startup.sh
+++ scripts/raspberry_pi_on_startup.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+# Usage:
+# Give this file execute permissions
+# chmod +x path/to/raspberry_pi_on_startup.sh
+# Then do the following on startup of raspberry pi (https://unix.stackexchange.com/questions/166473/debian-how-to-run-a-script-on-startup-as-soon-as-there-is-an-internet-connecti):
+# 1. source the .env file to get $PROJECT_DIR and $RASPBERRY_PI_USER
+#    source /path/to/.env
+# 2. run this file
+#    bash -c "${PROJECT_DIR}/scripts/raspberry_pi_on_startup.sh > /path/to/run.log 2>&1"
+
 echo "-- INITIALIZING --"
 
 # Pull any new changes
 echo "** Pulling latest changes from bandcamp_suggestor"
 say "Updating"
-sudo /home/lucw/Documents/bandcamp_suggestor/bandcamp/bin/python3 /home/lucw/Documents/bandcamp_suggestor/scripts/wait_for_network.py
-sudo git -C /home/lucw/Documents/bandcamp_suggestor/ pull origin main
+sudo "${PROJECT_DIR}/bandcamp/bin/python3" "${PROJECT_DIR}/scripts/wait_for_network.py"
+sudo -u $RASPBERRY_PI_USER -i git -C $PROJECT_DIR pull origin main
 
 # Run the script
 echo "** Running bandcamp_suggestor"
 say "Starting radio"
-sudo /home/lucw/Documents/bandcamp_suggestor/bandcamp/bin/python3 /home/lucw/Documents/bandcamp_suggestor/scripts/play_recommendation.py &
+sudo "${PROJECT_DIR}/bandcamp/bin/python3" "${PROJECT_DIR}/scripts/play_recommendation.py" &
