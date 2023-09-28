1f26d2399210348da227972a5140d8a1beedc639
lucweytingh <lucweytingh321@gmail.com>
2023-09-29 00:51:39 +0200

    no auto-update

--- scripts/raspberry_pi_on_startup.sh
+++ scripts/raspberry_pi_on_startup.sh
@@ -10,15 +10,7 @@
 #    bash -c "${PROJECT_DIR}/scripts/raspberry_pi_on_startup.sh > /path/to/run.log 2>&1"
 
 echo "-- INITIALIZING --"
-
-# Pull any new changes
-echo "** Pulling latest changes from bandcamp_suggestor"
-say "Updating"
-sudo "${PROJECT_DIR}bandcamp/bin/python3" "${PROJECT_DIR}scripts/wait_for_network.py"
-sudo find "${PROJECT_DIR}.git/objects/" -size 0 -exec rm -f {} \; # Fixes git if broken
-sudo -u $RASPBERRY_PI_USER -i git -C $PROJECT_DIR fetch origin # Fixes git if broken
-sudo -u $RASPBERRY_PI_USER -i git -C $PROJECT_DIR pull origin main
-sudo "${PROJECT_DIR}bandcamp/bin/python3 -m pip install -r ${PROJECT_DIR}requirements.txt"
+sudo ${PROJECT_DIR}bandcamp/bin/python3 ${PROJECT_DIR}scripts/wait_for_network.py
 
 # Run the script
 echo "** Running bandcamp_suggestor"
